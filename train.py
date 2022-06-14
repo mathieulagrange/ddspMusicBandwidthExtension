07621475234303c01a8edc4d287d28dde54e0ba9
Pierre-Amaury Grumiaux <pierreamaury.grumiaux@gmail.com>
2022-06-14 08:06:58 +0200

    factor train.py with models.py

--- train.py
+++ train.py
@@ -1,105 +1,56 @@
 from data import OrchideaSol, MedleySolosDB
-from ddsp.training import train_util, models, trainers, encoders, decoders, preprocessing
+from ddsp.training import train_util, trainers, encoders, decoders, preprocessing
 from ddsp import losses
 from ddsp import processors
 from ddsp import synths
 from ddsp import core
 from ddsp import effects
+import ddsp
+from models import OriginalAutoencoder
 import matplotlib.pyplot as plt
+import logging
 
-### load dataset ###
-orchidea = OrchideaSol('test', sample_rate=16000, frame_rate=250, batch_size=1)
+# prepare log file
+logging.basicConfig(filename='test.log', filemode='w', level=logging.INFO, format='%(name)s - %(levelname)s - %(message)s')
+
+
+### dataset ###
+orchidea = OrchideaSol('train', sample_rate=16000, frame_rate=250, batch_size=1)
 dataset = orchidea.get_dataset()
 
 ### model ###
-ae = models.Autoencoder()
+model = OriginalAutoencoder()
 
-# preprocessor
-preprocessor = preprocessing.F0LoudnessPreprocessor(time_steps=1000)
-
-# encoder
-encoder = encoders.MfccTimeDistributedRnnEncoder(
-    rnn_channels = 512,
-    rnn_type = 'gru',
-    z_dims = 16,
-    z_time_steps = 250
-)
-
-# Decoder
-decoder = decoders.RnnFcDecoder(
-    rnn_channels = 512,
-    rnn_type = 'gru',
-    ch = 512,
-    layers_per_stack = 3,
-    input_keys = ('ld_scaled', 'f0_scaled', 'z'),
-    output_splits = (('amps', 1), ('harmonic_distribution', 100), ('noise_magnitudes', 65))
-)
-
-# Synths
-harmonic = synths.Harmonic(
-    n_samples = 64000,
-    sample_rate = 16000,
-    scale_fn = core.exp_sigmoid,
-    normalize_below_nyquist = True,
-    name = 'harmonic'
-)
-
-filtered_noise = synths.FilteredNoise(
-    n_samples = 64000,
-    window_size = 257,
-    scale_fn = core.exp_sigmoid,
-    name = 'filtered_noise'
-)
-
-harmonic_plus_fn= processors.Add(name='add')
-
-# Processor group
-dag = [
-    (harmonic, ['amps', 'harmonic_distribution', 'f0_hz']),
-    (filtered_noise, ['noise_magnitudes']),
-    (harmonic_plus_fn, ['filtered_noise/signal', 'harmonic/signal']),
-]
-processor_group = processors.ProcessorGroup(dag = dag, name='processor_group')
-
-# Reverb
-reverb = effects.Reverb()
-
-### Loss ###
-fft_sizes = [64, 128, 256, 512, 1024, 2048]
-spectral_loss = losses.SpectralLoss(
-    loss_type = 'L1',
-    mag_weight = 1.0,
-    logmag_weight = 1.0
-)
-
-### training loop ###
-batch_size = 1
+### training ###
+batch_size = 10
 save_dir = './results/ddsp/'
-dataset = orchidea.get_batch(batch_size=batch_size, shuffle=False).take(1).repeat()
+n_steps = 100
+early_stop_loss_value = None
 
+# create strategy
 strategy = train_util.get_strategy()
 with strategy.scope():
-    model = models.Autoencoder(preprocessor=preprocessor, 
-                                encoder=encoder, 
-                                decoder=decoder, 
-                                processor_group=processor_group,
-                                losses=[spectral_loss])
+    model = OriginalAutoencoder()
     trainer = trainers.get_trainer_class()(model, strategy, learning_rate=1e-3)
 
-batch = next(iter(dataset))
+# initiate dataset for training
+dataset = orchidea.get_batch(batch_size=batch_size, shuffle=True, repeats=-1)
 dataset = trainer.distribute_dataset(dataset)
 trainer.build(next(iter(dataset)))
-
 dataset_iter = iter(dataset)
 
-for i in range(30):
-  losses = trainer.train_step(dataset_iter)
-  res_str = 'step: {}\t'.format(i)
-  for k, v in losses.items():
-    res_str += '{}: {:.2f}\t'.format(k, v)
-  print(res_str)
+# training loop
+for i in range(n_steps):
+    losses = trainer.train_step(dataset_iter)
+    res_str = 'step: {}\t'.format(i)
+    for k, v in losses.items():
+        res_str += '{}: {:.2f}\t'.format(k, v)
+    print(res_str)
 
-# train_util.train(data_provider=orchidea,
-#                     trainer=trainer,
-#                     save_dir=save_dir,
-#                     restore_dir=save_dir)
+# Stop the training when the loss reaches given value
+    if (early_stop_loss_value is not None and
+        losses['total_loss'] <= early_stop_loss_value):
+        break
+
+# save model
+trainer.save(save_dir)
