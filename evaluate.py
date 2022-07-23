be8f08ca0cd6db75dd820291673eb35ad1ca1bce
Pierre-Amaury Grumiaux <pierreamaury.grumiaux@gmail.com>
2022-07-23 11:38:21 +0200

    enable training in multiple checkpoints

--- evaluate.py
+++ evaluate.py
@@ -1,4 +1,3 @@
-# from datasets import OrchideaSOLDataset, GtzanDataset#, NsynthDataset, MedleyDBSoloDataset
 from data import OrchideaSol, OrchideaSolTiny, MedleySolosDB, Gtzan
 from metrics import sdr, lsd
 from sbr import sbr
@@ -11,25 +10,28 @@ import os
 import customPath
 import tensorflow as tf
 import training
+import logging
+from models import OriginalAutoencoder
+from generate import generate_audio, checkpoint_test_generation
 
 def evaluate(setting, experiment):
     tic = time.time()
 
-    # dataset instantiation
+    # test dataset instantiation
     if setting.data == 'sol':
-        dataset = OrchideaSol('test', 16000, 250)
+        dataset_test = OrchideaSol('test', 4, 16000, 250)
     elif setting.data == 'tiny':
-        dataset = OrchideaSolTiny('test', 16000, 250)
+        dataset_test = OrchideaSolTiny('test', 4, 16000, 250)
     elif setting.data == 'medley':
-        dataset = MedleySolosDB('test', 16000, 250)
+        dataset_test = MedleySolosDB('test', 4, 16000, 250)
     elif setting.data == 'gtzan':
-        dataset = Gtzan('test', 16000, 250)
+        dataset_test = Gtzan('test', 4, 16000, 250)
     
-    ds = dataset.get_dataset()
+    ds_test = dataset_test.get_dataset()
     
     # how may examples in dataset
     ds_length = 0
-    for ex in ds:
+    for ex in ds_test.batch(batch_size=1):
         ds_length += 1
 
     # prepare metrics for each example
@@ -47,7 +49,7 @@ def evaluate(setting, experiment):
         else:
             harmonic_duplication = False
 
-        for i, test_data in tqdm(enumerate(ds)):
+        for i, test_data in tqdm(enumerate(ds_test)):
             audio = test_data['audio'].numpy()
 
             ### ORACLE ALGO ###
@@ -114,31 +116,75 @@ def evaluate(setting, experiment):
             all_lsd[i] = cur_lsd
 
     else:
-    ### DDSP ALGO ###
-        model_name = 'ddsp'
+        ### DDSP ALGO ###
+        model_name = 'ddsp_longTraining'
         model_dir = os.path.join(customPath.models(), model_name)
+        if not os.path.isdir(model_dir):
+            os.mkdir(model_dir)
+        model = OriginalAutoencoder()
+        logging.basicConfig(filename=os.path.join(model_dir, 'training.log'), level=logging.INFO, format='%(name)s - %(asctime)s - %(message)s')
 
-        # if a training has already been started for this model
-        if os.path.isdir(os.path.join(customPath.models(), model_name)):
-            n_steps_total = setting.n_steps_total
-            latest_checkpoint = tf.train.latest_checkpoint(model_dir)
+        n_steps_total = int(setting.n_steps_total)
+        if os.path.isdir(os.path.join(model_dir, 'train_files')):
+            latest_checkpoint = tf.train.latest_checkpoint(os.path.join(model_dir, 'train_files'))
             if latest_checkpoint is not None:
-                latest_checkpoint_n_steps = os.path.basename(latest_checkpoint).split('-')[0]
+                latest_checkpoint_n_steps = int(os.path.basename(latest_checkpoint).split('-')[-1])
             else:
                 latest_checkpoint_n_steps = 0
+
             # check if the training is completely done
             if latest_checkpoint_n_steps < setting.n_steps_total:
             # we train the model again for setting.n_steps_per_training steps
-                training.train(model_name, model_dir, setting)
-                # generateSomeOutputs()
+                logging.info(f'Training is restarted from step {latest_checkpoint_n_steps} out of n_steps_total.')
+                
+                training.train(model_name, os.path.join(model_dir, 'train_files'), setting)
+                logging.info('Generating reconstructed audio from some test data ...')
+                checkpoint_test_generation(model_dir, model, 'sol', latest_checkpoint_n_steps+setting.n_steps_per_training)
+                logging.info('Reconstruction done.')
+            
             else:
-                # generateSomeOutputs()
-                # computeMetrics()
-                raise NotImplementedError
+                # all training steps have been completed
+                logging.info('The training has reached the maximum number of steps.')
+                logging.info('Generating reconstructed audio from some test data ...')
+                checkpoint_test_generation(model_dir, model, 'sol', latest_checkpoint_n_steps+setting.n_steps_per_training)
+                logging.info('Reconstruction done.')
+                
+                # compute metrics for the whole test dataset
+                tic = time.time()
+                ds_test = ds_test.batch(batch_size=1)
+                model.restore(os.path.join(model_dir, 'train_files'))
+                for i_batch, batch in enumerate(ds_test):
+                    outputs = model(batch, training=False)
+
+                    reconstructed_audio = model.get_audio_from_outputs(outputs).numpy()[0]
+                    print(reconstructed_audio)
+                    reconstructed_stft = lr.stft(reconstructed_audio, n_fft = setting.nfft, hop_length = setting.nfft//2)
+
+                    audio = batch['audio_WB'].numpy()
+                    stft = lr.stft(audio, n_fft = setting.nfft, hop_length = setting.nfft//2)
+
+                    cur_sdr = sdr(audio, reconstructed_audio)
+                    cur_lsd = lsd(stft, reconstructed_stft)
+
+                    all_sdr[i_batch] = cur_sdr
+                    all_lsd[i_batch] = cur_lsd
+                
+                toc = time.time()
+                elapsed_time = int(toc-tic)
+                logging.info('All metrics have been computed for the whole test dataset.')
+
+
         # if no training has ever been done for this model
         else:
-            os.mkdir(model_dir)
-            training.train(model_name, model_dir, setting)
+            latest_checkpoint_n_steps = 0
+            logging.info('No on-going trainings have been found.')
+            os.mkdir(os.path.join(model_dir, 'train_files'))
+            logging.info("Model dir have been created, with subdir 'train_files'.")
+            training.train(model_name, os.path.join(model_dir, 'train_files'), setting)
+
+            logging.info('Generating reconstructed audio from some test data ...')
+            checkpoint_test_generation(model_dir, model, 'sol', latest_checkpoint_n_steps+setting.n_steps_per_training)
+            logging.info('Reconstruction done.')
 
     toc = time.time()
     elapsed_time = int(toc-tic)
