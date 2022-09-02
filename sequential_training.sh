96be06d71461e2d9ce87d24871cb67c314f67c1b
Pierre-Amaury Grumiaux <pierreamaury.grumiaux@gmail.com>
2022-09-02 14:14:43 +0200

    more jobs

--- sequential_training.sh
+++ sequential_training.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-JID_JOB1=`sbatch $1.slurm | cut -d " " -f 4`
-JID_JOB2=`sbatch --dependency=afterok:$JID_JOB1 $1.slurm | cut -d " " -f 4`
-JID_JOB3=`sbatch --dependency=afterok:$JID_JOB2 $1.slurm | cut -d " " -f 4`
-JID_JOB4=`sbatch --dependency=afterok:$JID_JOB3 $1.slurm | cut -d " " -f 4`
-JID_JOB5=`sbatch --dependency=afterok:$JID_JOB4 $1.slurm | cut -d " " -f 4`
-JID_JOB6=`sbatch --dependency=afterok:$JID_JOB5 $1.slurm | cut -d " " -f 4`
-sbatch --dependency=afterok:$JID_JOB6 $1.slurm
+JID_JOB1=`sbatch ./slurm/$1.slurm | cut -d " " -f 4`
+JID_JOB2=`sbatch --dependency=afterok:$JID_JOB1 ./slurm/$1.slurm | cut -d " " -f 4`
+JID_JOB3=`sbatch --dependency=afterok:$JID_JOB2 ./slurm/$1.slurm | cut -d " " -f 4`
+JID_JOB4=`sbatch --dependency=afterok:$JID_JOB3 ./slurm/$1.slurm | cut -d " " -f 4`
+JID_JOB5=`sbatch --dependency=afterok:$JID_JOB4 ./slurm/$1.slurm | cut -d " " -f 4`
+JID_JOB6=`sbatch --dependency=afterok:$JID_JOB5 ./slurm/$1.slurm | cut -d " " -f 4`
+JID_JOB7=`sbatch --dependency=afterok:$JID_JOB6 ./slurm/$1.slurm | cut -d " " -f 4`
+JID_JOB8=`sbatch --dependency=afterok:$JID_JOB7 ./slurm/$1.slurm | cut -d " " -f 4`
+JID_JOB9=`sbatch --dependency=afterok:$JID_JOB8 ./slurm/$1.slurm | cut -d " " -f 4`
+sbatch --dependency=afterok:$JID_JOB9 ./slurm/$1.slurm
