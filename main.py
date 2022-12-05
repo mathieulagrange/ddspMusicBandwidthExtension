b1813678900221a8ecd938c4415fb22b16b5b31f
Pierre-Amaury Grumiaux <pierreamaury.grumiaux@gmail.com>
2022-12-05 15:10:15 +0100

    update settings

--- main.py
+++ main.py
@@ -3,7 +3,6 @@
 import numpy as np
 from metrics import sdr, lsd
 import doce
-import sys
 import customPath
 import os
 from utils import filter_out_inf_then_mean
@@ -20,38 +19,39 @@ experiment.set_path('output', os.path.join(customPath.results(),experiment.name,
 
 # experiment plan
 experiment.add_plan('sbr',
+    data = ['synthetic', 'sol', 'tiny', 'medley', 'dsd_sources', 'gtzan'],
     alg = ['sbr'],#, 'oracle', 'dumb'],
-    data = ['sol', 'tiny', 'medley', 'gtzan', 'synthetic'],
-    method = ['replication', 'harmonic', 'replication+harmonic'],
-    phase = ['oracle', 'flipped', 'noise'],
-    matchingEnergy = [0.25, 0.5, 1.0],
+    method = ['replication'],#, 'harmonic', 'replication+harmonic'],
+    phase = ['oracle'],#, 'flipped', 'noise'],
+    matchingEnergy = [0.5], #[0.25, 0.5, 1.0],
     nfft = [1024],
     sampling_rate = [16000],
     split = ['train', 'test']
     )
 
 experiment.add_plan('ddsp',
+    data = ['synthetic', 'sol', 'tiny', 'medley', 'dsd_sources', 'gtzan'],
     alg = ['ddsp'],
-    data = ['sol', 'tiny', 'medley', 'gtzan', 'synthetic'],
-    model = ['original_autoencoder'],
+    model = ['ddsp_original_autoencoder', 'resnet'],
     n_steps_total = [50000],
     n_steps_per_training = [5000],
     sampling_rate = [16000],
     split = ['train', 'test'],
-    nfft = [1024]
+    nfft = [1024],
+    loss = ['WB', 'HB']
     )
 
 # experiment metrics
 experiment.set_metric(
     name = 'sdr',
-    significance = True,
+    # significance = True,
     higher_the_better = True,
     func = filter_out_inf_then_mean
 )
 
 experiment.set_metric(
     name = 'lsd',
-    significance = True,
+    # significance = True,
     lower_the_better = True
 )
 
