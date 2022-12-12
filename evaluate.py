81e70849f589a02d00b787622ac43c75bc833ebc
Pierre-Amaury Grumiaux <pierreamaury.grumiaux@gmail.com>
2022-12-12 11:07:28 +0100

    concatenate test signals per filename

--- evaluate.py
+++ evaluate.py
@@ -24,7 +24,7 @@ def evaluate(setting, experiment):
     tic = time.time()
 
     # test dataset instantiation
-    if 'ddsp' in setting.model:
+    if not hasattr(setting, 'model') or 'ddsp' in setting.model:
         if setting.data == 'sol':
             if setting.split == 'train':
                 data_dir = os.path.join(customPath.orchideaSOL(), 'preprocessed_ddsp/train')
@@ -50,6 +50,12 @@ def evaluate(setting, experiment):
                 data_dir = os.path.join(customPath.dsd_sources(), 'preprocessed_ddsp/train')
             elif setting.split == 'test':
                 data_dir = os.path.join(customPath.dsd_sources(), 'preprocessed_ddsp/test')
+        elif setting.data == 'dsd_mixtures':
+            if setting.split == 'train':
+                data_dir = os.path.join(customPath.dsd_mixtures(), 'preprocessed_ddsp/train')
+            elif setting.split == 'test':
+                data_dir = os.path.join(customPath.dsd_mixtures(), 'preprocessed_ddsp/test')
+
     elif setting.model == 'resnet':
         if setting.data == 'synthetic':
             if setting.split == 'train':
@@ -71,6 +77,11 @@ def evaluate(setting, experiment):
                 data_dir = os.path.join(customPath.dsd_sources(), 'preprocessed_resnet/train')
             elif setting.split == 'test':
                 data_dir = os.path.join(customPath.dsd_sources(), 'preprocessed_resnet/test')
+        elif setting.data == 'dsd_mixtures':
+            if setting.split == 'train':
+                data_dir = os.path.join(customPath.dsd_mixtures(), 'preprocessed_resnet/train')
+            elif setting.split == 'test':
+                data_dir = os.path.join(customPath.dsd_mixtures(), 'preprocessed_resnet/test')
 
 
     if setting.alg == 'ddsp':
@@ -83,8 +94,8 @@ def evaluate(setting, experiment):
     dataloader = torch.utils.data.DataLoader(dataset, 1, False)
     
     # prepare metrics for each example
-    all_sdr = np.empty((len(dataloader)))
-    all_lsd = np.empty((len(dataloader)))
+    all_sdr = []
+    all_lsd = []
     
     if setting.alg != 'ddsp':
         if 'replication' in setting.method:
@@ -97,7 +108,56 @@ def evaluate(setting, experiment):
         else:
             harmonic_duplication = False
 
+        first_example = 1
+        orig_audio_list = []
+        rec_audio_list = []
+
         for i, test_data in tqdm(enumerate(dataloader)):
+            if setting.data == 'synthetic':
+                if not hasattr(setting, 'model') or setting.model == 'ddsp_original_autoencoder':
+                    filename_idx = dataset.filenames[i].split('/')[-1][:-4]
+                    chunk_idx = 0
+                elif setting.model == 'resnet':
+                    filename_idx = dataset.filenames[i][0].split('/')[-1][:-4]
+                    chunk_idx = dataset.filenames[i][1]
+            elif setting.data == 'sol':
+                filename_idx = dataset.filenames[i][0].split('/')[-1][:-4]
+                chunk_idx = dataset.filenames[i][1]
+            elif setting.data == 'medley':
+                filename_idx = dataset.filenames[i][0].split('/')[-1][:-4]
+                chunk_idx = dataset.filenames[i][1]
+            elif setting.data == 'dsd_sources':
+                filename_idx = dataset.filenames[i][0].split('/')[-1][:-4]
+                chunk_idx = dataset.filenames[i][1]
+            elif setting.data == 'dsd_mixtures':
+                filename_idx = dataset.filenames[i][0].split('/')[-1][:-4]
+                chunk_idx = dataset.filenames[i][1]
+
+            if chunk_idx == 0:
+                if not first_example:
+                    orig_audio_concat = np.concatenate(orig_audio_list)
+                    rec_audio_concat = np.concatenate(rec_audio_list)
+
+                    # original WB signal + stft
+                    orig_stft = lr.stft(orig_audio_concat, n_fft = setting.nfft, hop_length = setting.nfft//2)
+
+                    # recontructed stft
+                    rec_stft = lr.stft(rec_audio_concat, n_fft = setting.nfft, hop_length = setting.nfft//2)
+
+                    # we replace the LB with the ground-truth before computing metrics
+                    rec_stft[:ceil(setting.nfft//4), :] = orig_stft[:ceil(setting.nfft//4), :]
+
+                    # we compute metrics and store them
+                    cur_sdr = sdr(orig_audio_concat, rec_audio_concat)
+                    cur_lsd = lsd(orig_stft[ceil(setting.nfft//4):], rec_stft[ceil(setting.nfft//4):])
+                    all_sdr.append(cur_sdr)
+                    all_lsd.append(cur_lsd)
+
+                orig_audio_list = []
+                rec_audio_list = []
+                first_example = 0
+
+
             s_WB, s_LB, p, l = test_data
             audio = s_WB[0].detach().cpu().numpy()
 
@@ -107,8 +167,10 @@ def evaluate(setting, experiment):
                 stft = lr.stft(audio, n_fft = setting.nfft, hop_length = setting.nfft//2)
                 reconstructed_stft = lr.stft(reconstructed_audio, n_fft = setting.nfft, hop_length = setting.nfft//2)
 
-                cur_sdr = sdr(audio, reconstructed_audio)
-                cur_lsd = lsd(stft, reconstructed_stft, n_fft = setting.nfft, hop_length = setting.nfft//2)
+                orig_audio_list.append(audio)
+                rec_audio_list.append(reconstructed_audio)
+                # cur_sdr = sdr(audio, reconstructed_audio)
+                # cur_lsd = lsd(stft, reconstructed_stft, n_fft = setting.nfft, hop_length = setting.nfft//2)
 
             ### DUMB ALGO ###
             elif setting.alg == 'dumb':
@@ -128,8 +190,11 @@ def evaluate(setting, experiment):
                 elif reconstructed_audio.size > audio.size:
                     reconstructed_audio.size = reconstructed_audio[:audio.size]
 
-                cur_sdr = sdr(audio, reconstructed_audio)
-                cur_lsd = lsd(stft[ceil(setting.nfft//4):], reconstructed_stft[ceil(setting.nfft//4):], n_fft = setting.nfft, hop_length = setting.nfft//2)
+                orig_audio_list.append(audio)
+                rec_audio_list.append(reconstructed_audio)
+
+                # cur_sdr = sdr(audio, reconstructed_audio)
+                # cur_lsd = lsd(stft[ceil(setting.nfft//4):], reconstructed_stft[ceil(setting.nfft//4):], n_fft = setting.nfft, hop_length = setting.nfft//2)
 
             ### SBR ALGO ###
             elif setting.alg == 'sbr':
@@ -155,53 +220,101 @@ def evaluate(setting, experiment):
                 elif reconstructed_audio.size > audio.size:
                     reconstructed_audio.size = reconstructed_audio[:audio.size]
 
+                orig_audio_list.append(audio)
+                rec_audio_list.append(reconstructed_audio)
+
                 # we compute the metrics
-                cur_sdr = sdr(audio, reconstructed_audio)
-                cur_lsd = lsd(stft[ceil(setting.nfft//4):], reconstructed_stft[ceil(setting.nfft//4):])
+                # cur_sdr = sdr(audio, reconstructed_audio)
+                # cur_lsd = lsd(stft[ceil(setting.nfft//4):], reconstructed_stft[ceil(setting.nfft//4):])
             
             # we save the metrics for this test data
-            all_sdr[i] = cur_sdr
-            all_lsd[i] = cur_lsd
+            # all_sdr[i] = cur_sdr
+            # all_lsd[i] = cur_lsd
+
+        # last batch    
+        orig_audio_concat = np.concatenate(orig_audio_list)
+        rec_audio_concat = np.concatenate(rec_audio_list)
+
+        orig_stft = lr.stft(orig_audio_concat, n_fft = setting.nfft, hop_length = setting.nfft//2)
+        rec_stft = lr.stft(rec_audio_concat, n_fft = setting.nfft, hop_length = setting.nfft//2)
+        rec_stft[:ceil(setting.nfft//4), :] = orig_stft[:ceil(setting.nfft//4), :]
+
+        cur_sdr = sdr(orig_audio_concat, rec_audio_concat)
+        cur_lsd = lsd(orig_stft[ceil(setting.nfft//4):], rec_stft[ceil(setting.nfft//4):])
+        all_sdr.append(cur_sdr)
+        all_lsd.append(cur_lsd)
 
     else:
         ### DDSP ALGO ###               
         # model name
-        step = None
-        if setting.model == 'ddsp_original_autoencoder':
-            if setting.data == 'sol':
-                if setting.loss == 'WB':
-                    model_name = 'bwe_sol_100harmo_25000steps_batch32_lossWB'
-                else:
-                    model_name = 'bwe_sol_100harmo_25000steps_batch32_lossHB'
+        if setting.sampling_rate == 16000:
+            if setting.model == 'ddsp_original_autoencoder':
+                if setting.data == 'synthetic':
+                    if setting.loss == 'WB':
+                        model_name = 'bwe_synth_8000_100harmo_5000steps_batch32_lossWB'
+                    else:
+                        model_name = 'bwe_synth_8000_100harmo_5000steps_batch32_lossHB'
+                elif setting.data == 'sol':
+                    if setting.loss == 'WB':
+                        model_name = 'bwe_sol_8000_100harmo_25000steps_batch32_lossWB'
+                    else:
+                        model_name = 'bwe_sol_8000_100harmo_25000steps_batch32_lossHB'
+                elif setting.data == 'medley':
+                    if setting.loss == 'WB':
+                        model_name = 'bwe_medley_8000_100harmo_25000steps_batch32_lossWB'
+                    elif setting.loss == 'HB':
+                        model_name = 'bwe_medley_8000_100harmo_25000steps_batch32_lossHB'
+                elif setting.data == 'dsd_sources':
+                    if setting.loss == 'WB':
+                        model_name = 'bwe_dsd_sources_100harmo_25000steps_batch32_lossWB'
+                    elif setting.loss == 'HB':
+                        model_name = 'bwe_dsd_sources_100harmo_25000steps_batch32_lossHB'
+                elif setting.data == 'dsd_mixtures':
+                    if setting.loss == 'WB':
+                        model_name = 'bwe_dsd_mixtures_100harmo_25000steps_batch32_lossWB'
+                    elif setting.loss == 'HB':
+                        model_name = 'bwe_dsd_mixtures_100harmo_25000steps_batch32_lossHB'
 
-            elif setting.data == 'medley':
-                if setting.loss == 'WB':
-                    model_name = 'bwe_medley_100harmo_25000steps_batch32_lossWB'
-                elif setting.loss == 'HB':
-                    model_name = 'bwe_medley_100harmo_25000steps_batch32_lossHB'
+            elif setting.model == 'resnet':
+                if setting.data == 'synthetic':
+                    model_name = 'bwe_synth_resnet_64000steps_batch16'
+                elif setting.data == 'sol':
+                    model_name = 'bwe_sol_resnet_64000steps_batch8'
+                elif setting.data == 'medley':
+                    model_name = 'bwe_medley_resnet_250000steps_batch16'
+                elif setting.data == 'dsd_sources':
+                    model_name = 'bwe_dsd_sources_resnet_250000steps_batch8'
+                elif setting.data == 'dsd_mixtures':
+                    model_name = 'bwe_dsd_mixtures_resnet_250000steps_batch8'
+ 
+        elif setting.sampling_rate == 8000:
+            if setting.model == 'ddsp_original_autoencoder':
+                if setting.data == 'sol':
+                    if setting.loss == 'WB':
+                        model_name = 'bwe_sol_100harmo_25000steps_batch32_lossWB'
+                    else:
+                        model_name = 'bwe_sol_100harmo_25000steps_batch32_lossHB'
+                elif setting.data == 'medley':
+                    if setting.loss == 'WB':
+                        model_name = 'bwe_medley_100harmo_25000steps_batch32_lossWB'
+                    elif setting.loss == 'HB':
+                        model_name = 'bwe_medley_100harmo_25000steps_batch32_lossHB'
+                elif setting.data == 'synthetic':
+                    if setting.loss == 'WB':
+                        model_name = 'bwe_synth_100harmo_5000steps_batch32_lossWB'
+                    else:
+                        model_name = 'bwe_synth_100harmo_5000steps_batch32_lossHB'
+                elif setting.data == 'dsd_sources':
+                    if setting.loss == 'WB':
+                        model_name = 'bwe_dsd_sources_100harmo_25000steps_batch32_lossWB'
+                    elif setting.loss == 'HB':
+                        model_name = 'bwe_dsd_sources_100harmo_25000steps_batch32_lossHB'
+                elif setting.data == 'dsd_mixtures':
+                    if setting.loss == 'WB':
+                        model_name = 'bwe_dsd_mixtures_100harmo_25000steps_batch32_lossWB'
+                    elif setting.loss == 'HB':
+                        model_name = 'bwe_dsd_mixtures_100harmo_25000steps_batch32_lossHB'
 
-            elif setting.data == 'synthetic':
-                if setting.loss == 'WB':
-                    model_name = 'bwe_synth_100harmo_5000steps_batch32_lossWB'
-                else:
-                    model_name = 'bwe_synth_100harmo_5000steps_batch32_lossHB'
-
-            elif setting.data == 'dsd_sources':
-                if setting.loss == 'WB':
-                    model_name = 'bwe_dsd_sources_100harmo_25000steps_batch32_lossWB'
-                elif setting.loss == 'HB':
-                    model_name = 'bwe_dsd_sources_100harmo_25000steps_batch32_lossHB_3'
-
-        elif setting.model == 'resnet':
-            if setting.data == 'synthetic':
-                model_name = 'bwe_synth_resnet_64000steps_batch16'
-            elif setting.data == 'sol':
-                model_name = 'bwe_sol_resnet_64000steps_batch8'
-            elif setting.data == 'medley':
-                model_name = 'bwe_medley_resnet_250000steps_batch16'
-            elif setting.data == 'dsd_sources':
-                model_name = 'bwe_dsd_sources_resnet_250000steps_batch8'
-        
 
         # config file loading
         with open(os.path.join(customPath.models(), model_name, "config.yaml"), "r") as config:
@@ -219,11 +332,57 @@ def evaluate(setting, experiment):
             model.load_state_dict(torch.load(os.path.join(customPath.models(), model_name, "state.pth"), map_location=torch.device('cpu')))
             model.eval()
 
-
         print('Trained model loaded.')
 
         print('Evaluation on the whole test set ...')
+        first_example = 1
+        orig_audio_list = []
+        rec_audio_list = []
         for i_batch, batch in tqdm(enumerate(dataloader)):
+            if config['data']['dataset'] == 'synthetic':
+                if setting.model == 'ddsp_original_autoencoder':
+                    filename_idx = dataset.filenames[i_batch].split('/')[-1][:-4]
+                    chunk_idx = 0
+                elif setting.model == 'resnet':
+                    filename_idx = dataset.filenames[i_batch][0].split('/')[-1][:-4]
+                    chunk_idx = dataset.filenames[i_batch][1]
+            elif config['data']['dataset'] == 'sol':
+                filename_idx = dataset.filenames[i_batch][0].split('/')[-1][:-4]
+                chunk_idx = dataset.filenames[i_batch][1]
+            elif config['data']['dataset'] == 'medley':
+                filename_idx = dataset.filenames[i_batch][0].split('/')[-1][:-4]
+                chunk_idx = dataset.filenames[i_batch][1]
+            elif config['data']['dataset'] == 'dsd_sources':
+                filename_idx = dataset.filenames[i_batch][0].split('/')[-1][:-4]
+                chunk_idx = dataset.filenames[i_batch][1]
+            elif config['data']['dataset'] == 'dsd_mixtures':
+                filename_idx = dataset.filenames[i_batch][0].split('/')[-1][:-4]
+                chunk_idx = dataset.filenames[i_batch][1]
+
+            if chunk_idx == 0:
+                if not first_example:
+                    orig_audio_concat = np.concatenate(orig_audio_list)
+                    rec_audio_concat = np.concatenate(rec_audio_list)
+
+                    # original WB signal + stft
+                    orig_stft = lr.stft(orig_audio_concat, n_fft = setting.nfft, hop_length = setting.nfft//2)
+
+                    # recontructed stft
+                    rec_stft = lr.stft(rec_audio_concat, n_fft = setting.nfft, hop_length = setting.nfft//2)
+
+                    # we replace the LB with the ground-truth before computing metrics
+                    rec_stft[:ceil(setting.nfft//4), :] = orig_stft[:ceil(setting.nfft//4), :]
+
+                    # we compute metrics and store them
+                    cur_sdr = sdr(orig_audio_concat, rec_audio_concat)
+                    cur_lsd = lsd(orig_stft[ceil(setting.nfft//4):], rec_stft[ceil(setting.nfft//4):])
+                    all_sdr.append(cur_sdr)
+                    all_lsd.append(cur_lsd)
+
+                orig_audio_list = []
+                rec_audio_list = []
+                first_example = 0
+
             # output generation from the ddsp model
             if 'ddsp' in setting.model:
                 s_WB, s_LB, p, l = batch
@@ -241,26 +400,31 @@ def evaluate(setting, experiment):
                 y = model(s_LB)[0]
                 s_WB = s_WB[0]
 
-            # reconstructed signal + recontructed stft
-            reconstructed_audio = y[0].detach().cpu().numpy()
-            reconstructed_stft = lr.stft(reconstructed_audio, n_fft = setting.nfft, hop_length = setting.nfft//2)
-
-            # original WB signal + stft
+            rec_audio = y[0].detach().cpu().numpy()
             orig_audio = s_WB[0].detach().cpu().numpy()
-            orig_stft = lr.stft(orig_audio, n_fft = setting.nfft, hop_length = setting.nfft//2)
+            
+            orig_audio_list.append(orig_audio)
+            rec_audio_list.append(rec_audio)
 
-            # we replace the LB with the ground-truth before computing metrics
-            reconstructed_stft[:ceil(setting.nfft//4), :] = orig_stft[:ceil(setting.nfft//4), :]
+        # last batch    
+        orig_audio_concat = np.concatenate(orig_audio_list)
+        rec_audio_concat = np.concatenate(rec_audio_list)
 
-            # we compute metrics and store them
-            cur_sdr = sdr(orig_audio, reconstructed_audio)
-            cur_lsd = lsd(orig_stft[ceil(setting.nfft//4):], reconstructed_stft[ceil(setting.nfft//4):])
-            all_sdr[i_batch] = cur_sdr
-            all_lsd[i_batch] = cur_lsd
+        orig_stft = lr.stft(orig_audio_concat, n_fft = setting.nfft, hop_length = setting.nfft//2)
+        rec_stft = lr.stft(rec_audio_concat, n_fft = setting.nfft, hop_length = setting.nfft//2)
+        rec_stft[:ceil(setting.nfft//4), :] = orig_stft[:ceil(setting.nfft//4), :]
+
+        cur_sdr = sdr(orig_audio_concat, rec_audio_concat)
+        cur_lsd = lsd(orig_stft[ceil(setting.nfft//4):], rec_stft[ceil(setting.nfft//4):])
+        all_sdr.append(cur_sdr)
+        all_lsd.append(cur_lsd)
 
         print('Evaluation done.')
             
     toc = time.time()
     elapsed_time = int(toc-tic)
+
+    all_sdr = np.array(all_sdr)
+    all_lsd = np.array(all_lsd)
 
     return all_sdr, all_lsd, elapsed_time
